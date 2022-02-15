7f65fbbf1d458df7755910d32fd8b889964f18a0
Luminare7 <amos@amosbocelli.com>
2022-02-15 19:05:04 +0100

    Snake_I

---
+++ Day20/main.py
@@ -0,0 +1,29 @@
+# Welcome to Day20!
+# Making the famous snake game.
+from turtle import Screen
+import time
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(fun=snake.up, key="Up")
+screen.onkey(fun=snake.down, key="Down")
+screen.onkey(fun=snake.left, key="Left")
+screen.onkey(fun=snake.right, key="Right")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()  # The screen refreshes every 0.1 seconds
+    time.sleep(0.1)
+
+    snake.move()
+
+screen.exitonclick()
